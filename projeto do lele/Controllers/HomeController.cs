37c5b56effc8dd05d286597f0f0e3702b65c6196
leitidev <leiticontato@hotmail.com>
2024-06-03 13:54:30 -0300

    EventosAdm feito

--- projeto do lele/Controllers/HomeController.cs
+++ projeto do lele/Controllers/HomeController.cs
@@ -38,6 +38,11 @@ namespace projeto_do_lele.Controllers
             return View();
         }
 
+        public IActionResult EventosAdm()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
